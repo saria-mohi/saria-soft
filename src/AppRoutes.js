6daba3b82bf0c008272fc60bd51b56f97ec4f1c1
saria-mohi <saria.m.mohammed@gmail.com>
2023-11-29 18:23:23 +0300

    bulid online

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Users from './pages/Users/Users';
 import Profile from './pages/Profile/Profile';
@@ -17,22 +19,39 @@ import UserProfile from './component/UserProfile';
 function AppRoutes() {
   return (
     <Router>
-     <Routes>
-     <Route path="/" element={<WelcomePage />} />
-     <Route path="/Dashboard" element={<Dashboard />} />
+    <Routes>
+    <Route path="/" element={<WelcomePage />} />
+      <Route path="/about" element={<Dashboard />} />
       <Route path="/users" element={<Users />} />
-      <Route path="/Profile" element={<Profile />} />
-      <Route path="/Tasks" element={<Tasks />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/tasks" element={<Tasks />} />
       <Route path="/edit-task" element={<TaskOperation />} />
-      <Route path="/Messages" element={<Messages />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Signup" element={<Signup />} />
-      <Route path="/Profile" element={<Profile />} />
-      <Route path="/UserProfile" element={<UserProfile />} />
-
-      {/* ... set up other routes */}
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/user-profile" element={<UserProfile />} />
+      
+      {/* other routes */}
     </Routes>
-    </Router>
+  </Router>
+    // <Router>
+    //  <Routes>
+    //  <Route path="/" element={<WelcomePage />} />
+    //  <Route path="/Dashboard" element={<Dashboard />} />
+    //   <Route path="/users" element={<Users />} />
+    //   <Route path="/Profile" element={<Profile />} />
+    //   <Route path="/Tasks" element={<Tasks />} />
+    //   <Route path="/edit-task" element={<TaskOperation />} />
+    //   <Route path="/Messages" element={<Messages />} />
+    //   <Route path="/Login" element={<Login />} />
+    //   <Route path="/Signup" element={<Signup />} />
+    //   <Route path="/Profile" element={<Profile />} />
+    //   <Route path="/UserProfile" element={<UserProfile />} />
+
+    //   {/* ... set up other routes */}
+    // </Routes>
+    // </Router>
   );
 }
 
